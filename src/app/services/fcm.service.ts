0ee524eb027a3ed6dfdff06d23f6bdaa19f6d433
Rudi Strydom <iam@thatguy.co.za>
2017-08-02 19:31:17 +0200

    Only trying to use notifications when available

--- src/app/services/fcm.service.ts
+++ src/app/services/fcm.service.ts
@@ -2,30 +2,35 @@ import { Inject, Injectable } from "@angular/core";
 import { FirebaseApp } from "angularfire2";
 import * as firebase from 'firebase';
 
+declare var window: any;
+
 @Injectable()
 export class FcmService {
   private _messaging: firebase.messaging.Messaging;
 
   constructor(@Inject(FirebaseApp) private _firebaseApp: firebase.app.App) {
 
-    this._messaging = firebase.messaging(this._firebaseApp);
-    this._messaging.requestPermission()
-      .then(success => {
-        console.log("Permission Granted", success);
-        this.getToken('New Token');
-      })
-      .catch(error => { 
-        console.error("Notifications disabled", error);
-        this.clear();
+    // It's only available for Devices with Notifications API
+    if (window.hasOwnProperty('Notification')) {
+      this._messaging = firebase.messaging(this._firebaseApp);
+      this._messaging.requestPermission()
+        .then(success => {
+          console.log("Permission Granted", success);
+          this.getToken('New Token');
+        })
+        .catch(error => {
+          console.error("Notifications disabled", error);
+          this.clear();
+        });
+
+      this._messaging.onTokenRefresh(() => {
+        this.getToken('Refreshed Token');
       });
 
-    this._messaging.onTokenRefresh(() => {
-      this.getToken('Refreshed Token');
-    });
-
-    this._messaging.onMessage((payload) => {
-      console.log("Message received. ", payload);
-    });
+      this._messaging.onMessage((payload) => {
+        console.log("Message received. ", payload);
+      });
+    }
   }
 
   setToken(token) {
