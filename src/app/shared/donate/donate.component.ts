38ecb1ff8f420028b4be7d4e8bf74cfaf6c9d2a1
Rudi Strydom <iam@thatguy.co.za>
2018-05-19 23:05:27 +0200

    Donation screens added

--- src/app/shared/donate/donate.component.ts
+++ src/app/shared/donate/donate.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { TokenRingService } from '../../services/token-ring.service';
 import { environment } from '../../../environments/environment';
 
@@ -9,7 +9,17 @@ import { environment } from '../../../environments/environment';
 })
 export class DonateComponent implements OnInit {
 
-  public data = {};
+  public data = {
+    reference: false
+  };
+  public paid = false;
+  public progress = '';
+  public progressTime = 0;
+  public pollingInMs = 10;
+  public intervalTimeInMs = 5000;
+  public donationAmount = 10;
+  @Input() open = false;
+
   constructor(private api: TokenRingService) {
     // "donate": "https://api.tvtracker.co.za/donate",
     // "donate-check-paid": "https://api.tvtracker.co.za/donate/check_paid",
@@ -17,13 +27,40 @@ export class DonateComponent implements OnInit {
   }
 
   donationData() {
-    console.log("DONATION ENDPOINT", environment.endpoint['donate']);
-    this.api.apiCall(environment.endpoint['donate']).subscribe(res => {
+    this.api.apiCall(environment.endpoint['donate'] + "/" + this.donationAmount).subscribe(res => {
       this.data = res.data;
     });
   }
 
+  checkPaid() {
+    if (!this.open) return false;
+    this.api.apiCall(environment.endpoint['donate-check-paid'] + "/" + this.data.reference).subscribe(res => {
+      this.paid = res.data.paid;
+    });
+  }
+
+  indicateProgress() {
+    setTimeout(() => {
+      if (this.progressTime > this.intervalTimeInMs) {
+        this.checkPaid();
+        this.progressTime = 0;
+      } else {
+        this.progressTime += this.pollingInMs
+      }
+      this.progress = ((this.progressTime / this.intervalTimeInMs) * 100) + '%'
+      if (this.open) this.indicateProgress()
+    }, this.pollingInMs);
+  }
+
   ngOnInit() {
     this.donationData();
+    this.indicateProgress();
+  }
+
+  ngOnChanges(event) {
+    if (this.open) {
+      this.progressTime = 0;
+      this.indicateProgress();
+    }
   }
 }
